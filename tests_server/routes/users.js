8d2b5ec68542be708f5bcaffd4c56bb4b8c73107
José Rocha <jose.bernardo.rocha@gmail.com>
2019-03-19 15:39:31 +0000

    only keeping one user

--- tests_server/routes/users.js
+++ tests_server/routes/users.js
@@ -1,6 +1,5 @@
 var records = [
     { id: 1, username: 'jack', password: 'secret', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] }
-  , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
 ];
 
 exports.findById = function(id, cb) {
