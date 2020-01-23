1cfee453ac314020918fe88c7c088055309a5d46
Antonio Rito Silva <rito.silva@tecnico.ulisboa.pt>
2020-01-23 11:01:34 +0000

    question ui tests

--- frontend/tests/unit/views/teacher/questions/ShowQuestionDialogTest.spec.ts
+++ frontend/tests/unit/views/teacher/questions/ShowQuestionDialogTest.spec.ts
@@ -1,7 +1,7 @@
 import { shallowMount, Wrapper } from "@vue/test-utils";
 import ShowQuestionDialog from "@/views/teacher/questions/ShowQuestionDialog.vue";
 import ShowQuestion from "@/views/teacher/questions/ShowQuestion.vue";
-import question from "../../../samples/Question";
+import { questionWithFigure } from "../../../samples/Question";
 import Vue from "vue";
 import Vuetify from "vuetify";
 
@@ -13,13 +13,16 @@ describe("ShowQuestionDialog.vue with question", () => {
     Vue.use(Vuetify);
 
     wrapper = shallowMount(ShowQuestionDialog, {
-      propsData: { question, dialog }
+      propsData: { question: questionWithFigure, dialog: dialog }
     });
   });
 
   it("show a question", () => {
-    expect(wrapper.find(".headline").text()).toMatch(question.title);
+    expect(wrapper.html()).toContain(questionWithFigure.title);
+    expect(wrapper.find("span").text()).toMatch(questionWithFigure.title);
+    expect(wrapper.find(".headline").text()).toMatch(questionWithFigure.title);
+    expect(wrapper.contains({ name: "v-card" })).toBe(true);
+    expect(wrapper.vm.$props.dialog).toBe(true);
     expect(wrapper.find(ShowQuestion).exists()).toBe(true);
-    // expect(wrapper.find({ name: "v-card" });
   });
 });
