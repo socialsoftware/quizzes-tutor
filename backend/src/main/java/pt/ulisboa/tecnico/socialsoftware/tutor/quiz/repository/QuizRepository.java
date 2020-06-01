17e9ac02a2077653e520f7c9f4c39505486e51c9
Antonio Rito Silva <rito.silva@tecnico.ulisboa.pt>
2020-06-01 16:12:22 +0100

    fix

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/quiz/repository/QuizRepository.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/quiz/repository/QuizRepository.java
@@ -17,7 +17,7 @@ public interface QuizRepository extends JpaRepository<Quiz, Integer> {
     @Query(value = "SELECT * FROM quizzes q, course_executions c WHERE c.id = q.course_execution_id AND c.id = :executionId ORDER BY c.id", nativeQuery = true)
     List<Quiz> findQuizzesOfExecution(int executionId);
 
-    @Query(value = "SELECT q FROM Quiz q WHERE q.courseExecution.id = :executionId AND q.availableDate < :now AND (q.conclusionDate IS NULL OR q.conclusionDate > :now) AND q.type <> 'GENERATED' AND q.qrCodeOnly = false")
+    @Query(value = "SELECT q FROM Quiz q WHERE q.courseExecution.id = :executionId AND q.availableDate < :now AND (q.conclusionDate IS NULL OR q.conclusionDate < :now) AND q.type <> 'GENERATED' AND q.qrCodeOnly = false")
     List<Quiz> findAvailableNonGeneratedNonQRCodeOnlyQuizzes(int executionId, LocalDateTime now);
 
     @Query(value = "SELECT MAX(key) FROM quizzes", nativeQuery = true)
