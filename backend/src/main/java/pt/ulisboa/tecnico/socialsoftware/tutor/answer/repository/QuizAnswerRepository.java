c31edf55496f81f0fffe85b72ff6a96041f41bb8
Antonio Rito Silva <rito.silva@tecnico.ulisboa.pt>
2020-06-15 12:50:04 +0100

    fix bug

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/answer/repository/QuizAnswerRepository.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/answer/repository/QuizAnswerRepository.java
@@ -19,7 +19,7 @@ public interface QuizAnswerRepository extends JpaRepository<QuizAnswer, Integer>
     @Query(value = "SELECT * FROM quiz_answers qa JOIN quizzes q ON qa.quiz_id = q.id WHERE qa.completed AND NOT qa.used_in_statistics", nativeQuery = true)
     Set<QuizAnswer> findQuizAnswersToCalculateStatistics(LocalDateTime now);
 
-    @Query(value = "SELECT qa.quiz.id FROM QuizAnswer qa JOIN qa.quiz q WHERE qa.user.id = :userId AND q.courseExecution.id = :executionId AND (qa.completed = true OR (q.type = 'IN_CLASS' AND q.creationDate IS NOT NULL))")
+    @Query(value = "SELECT qa.quiz.id FROM QuizAnswer qa JOIN qa.quiz q WHERE qa.user.id = :userId AND q.courseExecution.id = :executionId AND (qa.completed = true OR (q.type = 'IN_CLASS' AND qa.creationDate IS NOT NULL))")
     Set<Integer> findClosedQuizAnswersQuizIds(int userId, int executionId);
 
     @Query(value = "SELECT qa FROM QuizAnswer qa JOIN qa.quiz q WHERE qa.user.id = :userId AND q.courseExecution.id = :executionId AND qa.completed = FALSE AND (q.type = 'GENERATED' OR q.qrCodeOnly = true)")
