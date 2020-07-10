9cf6f5d5be85717dd5d85c55be9e2595ea2f77d9
Manuel Goulão <msilvagoulao@gmail.com>
2020-07-10 13:00:40 +0100

    start confirm registration frontend

---
+++ frontend/src/views/RegistrationConfirmationView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <password-card :title='TITLE' email='placeholder@email.com' @onSubmit='confirmRegistration'></password-card>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import PasswordCard from '@/components/auth/PasswordCard.vue';
+
+@Component({
+  components: { PasswordCard }
+})
+export default class RegistrationConfirmationView extends Vue {
+	TITLE = 'Registration Confirmation'
+
+	async created() {}
+	
+	confirmRegistration(password: string) {
+		console.log(`Confirm Registration with password: ${password}`);
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
