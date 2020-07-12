194b175a3e3624bb3f5d5f25fe52631492b4505b
Tiago Fonseca <tiagoatfonseca@gmail.com>
2020-07-12 16:15:26 +0100

    Tdp_1: Web Services closes #3

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentController.java
@@ -0,0 +1,57 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import pt.ulisboa.tecnico.socialsoftware.tutor.config.DateHandler;
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto.TournamentDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+import java.util.Set;
+
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.AUTHENTICATION_ERROR;
+
+@RestController
+public class TournamentController {
+
+    @Autowired
+    private TournamentService tournamentService;
+
+    @PostMapping(value = "/tournaments")
+    @PreAuthorize("hasRole('ROLE_STUDENT')")
+    public TournamentDto createTournament(Principal principal, @Valid @RequestBody TournamentDto tournamentDto, @RequestParam Set<Integer> topicsId) {
+        User user = (User) ((Authentication) principal).getPrincipal();
+
+        if(user == null){
+            throw new TutorException(AUTHENTICATION_ERROR);
+        }
+
+        formatDates(tournamentDto);
+        return tournamentService.createTournament(user.getId(), topicsId, tournamentDto);
+    }
+
+    @GetMapping(value = "/tournaments/getAllTournaments")
+    @PreAuthorize("hasRole('ROLE_TEACHER') or hasRole('ROLE_STUDENT') or hasRole('ROLE_ADMIN')")
+    public List<TournamentDto> getAllTournaments(Principal principal) {
+        User user = (User) ((Authentication) principal).getPrincipal();
+
+        if(user == null){
+            throw new TutorException(AUTHENTICATION_ERROR);
+        }
+
+        return tournamentService.getAllTournaments(user);
+    }
+
+    private void formatDates(TournamentDto tournamentDto) {
+        if (tournamentDto.getStartTime() != null && !DateHandler.isValidDateFormat(tournamentDto.getStartTime()))
+            tournamentDto.setStartTime(DateHandler.toISOString(DateHandler.toLocalDateTime(tournamentDto.getStartTime())));
+
+        if (tournamentDto.getEndTime() !=null && !DateHandler.isValidDateFormat(tournamentDto.getEndTime()))
+            tournamentDto.setEndTime(DateHandler.toISOString(DateHandler.toLocalDateTime(tournamentDto.getEndTime())));
+    }
+}
