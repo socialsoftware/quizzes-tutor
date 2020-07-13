810044912477bbf0dcfc70481b7c5f10ca9b82a7
dserafim1999 <dserafim1999@gmail.com>
2020-07-13 14:53:02 +0100

    implemented page for QS_3 closes #47

--- frontend/src/views/student/submissions/ShowSubmissionDialog.vue
+++ frontend/src/views/student/submissions/ShowSubmissionDialog.vue
@@ -13,7 +13,16 @@
             <v-card-text class="text-left">
                 <show-question :question="submission.question" />
             </v-card-text>
-
+            <v-card-title>
+                <span class="headline">{{ 'Reviews History' }}</span>
+            </v-card-title>
+            <div class="text-left">
+                <show-reviews
+                        class="history"
+                        :key="reviewsComponentKey"
+                        :submission="submission"
+                />
+            </div>
             <v-card-actions>
                 <v-spacer />
                 <v-btn dark color="primary" data-cy="close" @click="$emit('dialog')"
@@ -25,17 +34,36 @@
 </template>
 
 <script lang="ts">
-  import { Component, Vue, Prop, Model } from 'vue-property-decorator';
+  import { Component, Vue, Prop, Model, Watch } from 'vue-property-decorator';
+  import ShowReviews from '@/views/student/submissions/ShowReviews.vue'
   import ShowQuestion from '@/views/student/submissions/ShowQuestion.vue';
-  import Submission from '../../../models/management/Submission';
+  import Submission from '@/models/management/Submission';
 
   @Component({
     components: {
-      'show-question': ShowQuestion
+      'show-question': ShowQuestion,
+      'show-reviews': ShowReviews
     }
   })
   export default class ShowSubmissionDialog extends Vue {
     @Model('dialog', Boolean) dialog!: boolean;
     @Prop({ type: Submission, required: true }) readonly submission!: Submission;
+
+    reviewsComponentKey: number = 0;
+
+    @Watch('dialog')
+    forceRerender() {
+      if (this.dialog) {
+        this.reviewsComponentKey += 1;
+      }
+    }
+
+
   }
 </script>
+<style lang="scss" scoped>
+    .history {
+        max-height: 235px;
+        overflow-y: auto;
+    }
+</style>
