59c0c9dcb5c96c7862b60f171bb8935ef7626748
dserafim1999 <dserafim1999@gmail.com>
2020-07-14 18:22:27 +0100

    implemented web service closes dserafim1999/quizzes-tutor/#38

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/submission/SubmissionController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/submission/SubmissionController.java
@@ -5,6 +5,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.submission.dto.ReviewDto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.submission.dto.SubmissionDto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
@@ -43,6 +44,12 @@ public class SubmissionController {
 
         reviewDto.setUserId(user.getId());
         return submissionService.createReview(reviewDto);
+    }
+
+    @PutMapping("/submissions/{submissionId}")
+    @PreAuthorize("hasRole('ROLE_STUDENT')")
+    public SubmissionDto updateSubmission(@PathVariable Integer submissionId, @Valid @RequestBody SubmissionDto submission) {
+        return this.submissionService.updateSubmission(submissionId, submission);
     }
 
     @PutMapping("/management/reviews/{questionId}")
