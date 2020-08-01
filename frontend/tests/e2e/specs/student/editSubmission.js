cc8837e10e2ade5f84b9952e282b5ca989d8e660
dserafim1999 <dserafim1999@gmail.com>
2020-08-01 18:07:06 +0100

    implemented end to end tests for QS_6 closes dserafim1999/quizzes-tutor#57

---
+++ frontend/tests/e2e/specs/student/editSubmission.js
@@ -0,0 +1,69 @@
+describe('Student walkthrough', () => {
+  beforeEach(() => {
+    cy.demoStudentLogin();
+
+    cy.get('[data-cy="submissionMenuButton"]').click();
+    cy.get('[data-cy="mySubmissionsMenuButton"]').click();
+  });
+
+  afterEach(() => {
+    cy.contains('Logout').click();
+  });
+
+  it('login edits a question', () => {
+    cy.submitQuestion(
+      true,
+      'Test',
+      'Test Question',
+      'A',
+      'B',
+      'C',
+      'D'
+    );
+
+    cy.editSubmission(
+      true,
+      ' 2',
+      ' 2',
+      '1',
+      '2',
+      '3',
+      '4'
+    );
+
+    cy.viewQuestion(
+      'Test 2',
+      'Test Question 2',
+      'A1',
+      'B2',
+      'C3',
+      'D4'
+    );
+    cy.deleteSubmission('Test 2', 5);
+  });
+
+  it('login edits an invalid question', () => {
+    cy.submitQuestion(
+      true,
+      'Test',
+      'Test Question',
+      'A',
+      'B',
+      'C',
+      'D'
+    );
+
+    cy.editSubmission(
+      false,
+      ' 2',
+      ' 2'
+    );
+
+    cy.closeErrorMessage('Error: Invalid content for option');
+
+    cy.log('close dialog');
+
+    cy.get('[data-cy="CancelButton"]').click();
+    cy.deleteSubmission('Test', 5);
+  });
+});
