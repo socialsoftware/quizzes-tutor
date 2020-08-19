a0c3ca7a2803834e1b859d7f2a6bd99850936aa1
Manuel Goulão <msilvagoulao@gmail.com>
2020-08-19 17:34:06 +0100

    spaces

--- upgrade/auth-users-dump.sql
+++ upgrade/auth-users-dump.sql
@@ -1,5 +1,5 @@
 ALTER TABLE Users ADD COLUMN Active boolean;
 UPDATE Users SET Active = CASE
-							  WHEN username LIKE 'ist%' THEN TRUE
-							  ELSE FALSE
-						  END;
+                              WHEN username LIKE 'ist%' THEN TRUE
+                              ELSE FALSE
+                          END;
