bb92abf2b5f6b6ec65fd148a88c35eacbefc78d8
jhmfreitas <joao.h.marques.de.freitas@tecnico.ulisboa.pt>
2020-08-21 16:34:55 +0100

    Setup for test works

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/service/CreateDiscussionTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/discussion/service/CreateDiscussionTest.groovy
@@ -2,20 +2,18 @@ package pt.ulisboa.tecnico.socialsoftware.tutor.discussion.service
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.context.annotation.Bean
 import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
 import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.QuestionAnswer
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.QuizAnswer
 import pt.ulisboa.tecnico.socialsoftware.tutor.config.DateHandler
-import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
-import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseDto
 import pt.ulisboa.tecnico.socialsoftware.tutor.discussion.dto.DiscussionDto
-import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.domain.Quiz
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.domain.QuizQuestion
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
 import spock.lang.Shared
-import spock.lang.Unroll
-import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.*
 
 @DataJpaTest
 class CreateDiscussionTest extends SpockTest {
@@ -23,6 +21,8 @@ class CreateDiscussionTest extends SpockTest {
     @Shared
     def student
     def question1
+    def quiz
+    def quizAnswer
 
     def setup(){
         student = new User(USER_1_NAME,USER_1_USERNAME, User.Role.STUDENT)
@@ -31,7 +31,41 @@ class CreateDiscussionTest extends SpockTest {
         question1 = new Question()
         question1.setCourse(course)
         question1.setTitle("Question title")
+        question1.setContent("Question Content")
         questionRepository.save(question1)
+
+        def quiz = new Quiz()
+        quiz.setKey(1)
+        quiz.setType("TEST")
+        quiz.setCourseExecution(courseExecutionRepository.findAll().get(0))
+        quizRepository.save(quiz)
+
+        def quizanswer = new QuizAnswer()
+        quizanswer.setUser(student)
+        quizanswer.setQuiz(quiz)
+        quizAnswerRepository.save(quizanswer)
+
+        def quizquestion = new QuizQuestion(quizRepository.findAll().get(0), question1, 3)
+        quizQuestionRepository.save(quizquestion)
+
+        def questionanswer = new QuestionAnswer()
+        questionanswer.setTimeTaken(1)
+        questionanswer.setQuizAnswer(quizanswer)
+        questionanswer.setQuizQuestion(quizquestion)
+        questionAnswerRepository.save(questionanswer)
+        questionanswer.setQuizQuestion(quizquestion)
+
+        quizquestion.addQuestionAnswer(questionAnswerRepository.findAll().get(0))
+
+        quizanswer.addQuestionAnswer(questionAnswerRepository.findAll().get(0))
+        quizanswer.setQuiz(quizRepository.findAll().get(0))
+
+        quiz.addQuizAnswer(quizAnswerRepository.findAll().get(0))
+        quiz.addQuizQuestion(quizQuestionRepository.findAll().get(0))
+        quiz.setCourseExecution(courseExecutionRepository.findAll().get(0))
+
+        quizanswer.setQuiz(quizRepository.findAll().get(0))
+        student.addQuizAnswer(quizAnswerRepository.findAll().get(0))
     }
 
     def "create discussion"(){
