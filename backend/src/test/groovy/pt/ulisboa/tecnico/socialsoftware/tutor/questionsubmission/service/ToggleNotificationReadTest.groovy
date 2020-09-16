be8d8908960676708fb2693f8f058f2c4308d335
Pedro Reganha <pedro.reganha@gmail.com>
2020-09-16 18:52:11 +0100

    Fixing ToggleNotificationReadTest

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/questionsubmission/service/ToggleNotificationReadTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/questionsubmission/service/ToggleNotificationReadTest.groovy
@@ -4,6 +4,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
 import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
 import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.MultipleChoiceQuestion
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Option
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
 import pt.ulisboa.tecnico.socialsoftware.tutor.questionsubmission.domain.QuestionSubmission
@@ -21,6 +22,7 @@ class ToggleNotificationReadTest extends SpockTest{
         student = new User(USER_1_NAME, USER_1_USERNAME, USER_1_EMAIL, User.Role.STUDENT, true, false)
         student.setEnrolledCoursesAcronyms(externalCourseExecution.getAcronym())
         userRepository.save(student)
+
         question = new Question()
         question.setKey(1)
         question.setTitle(QUESTION_1_TITLE)
@@ -28,12 +30,18 @@ class ToggleNotificationReadTest extends SpockTest{
         question.setCourse(externalCourse)
         question.setStatus(Question.Status.SUBMITTED)
         questionRepository.save(question)
+
+        def questionDetails = new MultipleChoiceQuestion()
+        question.setQuestionDetails(questionDetails)
+        questionDetailsRepository.save(questionDetails)
+
         optionOK = new Option()
         optionOK.setContent(OPTION_1_CONTENT)
         optionOK.setCorrect(true)
         optionOK.setSequence(0)
-        optionOK.setQuestion(question)
+        optionOK.setQuestionDetails(questionDetails)
         optionRepository.save(optionOK)
+
         questionSubmission = new QuestionSubmission()
         questionSubmission.setQuestion(question)
         questionSubmission.setSubmitter(student)
