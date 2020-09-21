a887246cde997a0f223193a30bd793b776e69a7d
Pedro Correia <pedro.correia.105@gmail.com>
2020-09-21 11:10:17 +0100

    Remove EnableWebMvc

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/config/WebConfiguration.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/config/WebConfiguration.java
@@ -1,14 +1,14 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-@EnableWebMvc
+@ComponentScan(basePackageClasses = { WebConfiguration.class })
 public class WebConfiguration extends WebMvcConfigurationSupport {
     private static final long MAX_AGE_SECS = 3600;
 
@@ -19,8 +19,8 @@ public class WebConfiguration extends WebMvcConfigurationSupport {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/questions/**").addResourceLocations("file:" + figuresDir);
 
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.25.0/");
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
