2c6eeced4b99e830e438dcc5ec50f4fed0a3c7a5
jhmfreitas <joao.h.marques.de.freitas@tecnico.ulisboa.pt>
2021-04-04 17:45:04 +0100

    Creates service modules

---
+++ backend/common/src/main/java/pt/ulisboa/tecnico/socialsoftware/common/App.java
@@ -0,0 +1,13 @@
+package pt.ulisboa.tecnico.socialsoftware.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ backend/execution/src/main/java/pt/ulisboa/tecnico/socialsoftware/execution/App.java
@@ -0,0 +1,13 @@
+package pt.ulisboa.tecnico.socialsoftware.execution;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
