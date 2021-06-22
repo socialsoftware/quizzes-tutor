ec7c84a2a8711b37d58b11931252d2a8e2fba937
Antonio Rito Silva <rito.silva@tecnico.ulisboa.pt>
2021-06-22 11:43:57 +0100

    upgrade versions

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/config/WebConfiguration.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/config/WebConfiguration.java
@@ -20,11 +20,11 @@ public class WebConfiguration extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/images/questions/**").addResourceLocations("file:" + figuresDir);
 
         registry.addResourceHandler("/swagger-ui/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.49.0/");
+                .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.50.0/");
 
 
         registry.addResourceHandler("/swagger/**")
-                    .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.49.0/");
+                    .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.50.0/");
 
 
         registry.addResourceHandler("/webjars/**")
