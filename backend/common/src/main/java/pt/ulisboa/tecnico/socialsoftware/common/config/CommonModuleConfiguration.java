da7e946670bdd4515fbcbc7c6410d45b43cb93ae
jhmfreitas <joao.h.marques.de.freitas@tecnico.ulisboa.pt>
2021-07-18 22:54:01 +0100

    Optimizes imports

--- backend/common/src/main/java/pt/ulisboa/tecnico/socialsoftware/common/config/CommonModuleConfiguration.java
+++ backend/common/src/main/java/pt/ulisboa/tecnico/socialsoftware/common/config/CommonModuleConfiguration.java
@@ -1,11 +1,8 @@
 package pt.ulisboa.tecnico.socialsoftware.common.config;
 
-import com.google.common.eventbus.EventBus;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import pt.ulisboa.tecnico.socialsoftware.common.remote.*;
 
 @Configuration
 @ComponentScan(basePackages = "pt.ulisboa.tecnico.socialsoftware.common")
