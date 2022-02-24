3cab8d2fa1d8479f87a69df113b4b9b255433775
alexpires7 <alexandresilvapires16@tecnico.ulisboa.pt>
2022-02-24 19:32:37 +0000

    Added base repository and service

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/FailedAnswerService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/FailedAnswerService.java
@@ -1,21 +1,52 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.dashboard;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.domain.Dashboard;
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.domain.FailedAnswer;
 import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.dto.FailedAnswerDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.repository.DashboardRepository;
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.repository.FailedAnswerRepository;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Student;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.repository.StudentRepository;
+import pt.ulisboa.tecnico.socialsoftware.tutor.utils.DateHandler;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage;
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Set;
 
 public class FailedAnswerService {
 
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private DashboardRepository dashboardRepository;
+
+    @Autowired
+    private FailedAnswerRepository failedAnswerRepository;
+
+
     @Retryable(
             value = {SQLException.class},
             backoff = @Backoff(delay = 5000))
     @Transactional(isolation = Isolation.READ_COMMITTED)
-    public List<FailedAnswerDto> updateFailedAnswers(int courseExecutionId, int userId) {
+    public List<FailedAnswerDto> updateFailedAnswers(int dashboardId) {
+
+        Dashboard dashboard = dashboardRepository.findById(dashboardId).orElseThrow(() -> new TutorException(DASHBOARD_NOT_FOUND, dashboardId));
+
+        //TODO map to failed answer?
+        Set<Integer> newFailedAnswers = failedAnswerRepository.findNewFailedAnswer(dashboardId, DateHandler.now());
+
+        dashboard.updateFailedAnswers(newFailedAnswers);
+
         return null;
     }
 
@@ -23,7 +54,39 @@ public class FailedAnswerService {
             value = {SQLException.class},
             backoff = @Backoff(delay = 5000))
     @Transactional(isolation = Isolation.READ_COMMITTED)
-    public List<FailedAnswerDto> getFailedAnswers(int courseExecutionId, int userId) {
+    public List<FailedAnswerDto> getFailedAnswers(int dashboardId) {
+
+        Dashboard dashboard = dashboardRepository.findById(dashboardId).orElseThrow(() -> new TutorException(DASHBOARD_NOT_FOUND, dashboardId));
+
+        return dashboard.getFailedAnswers();
+    }
+
+    @Retryable(
+            value = {SQLException.class},
+            backoff = @Backoff(delay = 5000))
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public void removeFailedAnswer(int failedAnswerId, int userId, int dashboardId) {
+
+        Dashboard dashboard = dashboardRepository.findById(dashboardId).orElseThrow(() -> new TutorException(DASHBOARD_NOT_FOUND, dashboardId));
+
+        FailedAnswer toRemove = failedAnswerRepository.findById(failedAnswerId).orElseThrow(() -> new TutorException(FAILED_ANSWER_NOT_FOUND, failedAnswerId));
+
+        dashboard.removeFailedAnswer(toRemove);
+        failedAnswerRepository.delete(toRemove);
+    }
+
+    @Retryable(
+            value = {SQLException.class},
+            backoff = @Backoff(delay = 5000))
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public List<FailedAnswerDto> reAddFailedAnswers(int courseExecutionId, int dashboardId, int userId, String startDate, String endDate) {
+        //TODO
+
+        Student student = studentRepository.findById(userId).orElseThrow(() -> new TutorException(USER_NOT_FOUND, userId));
+
+        Dashboard dashboard = dashboardRepository.findById(dashboardId).orElseThrow(() -> new TutorException(DASHBOARD_NOT_FOUND, dashboardId));
+
+
         return null;
     }
 
@@ -31,23 +94,13 @@ public class FailedAnswerService {
             value = {SQLException.class},
             backoff = @Backoff(delay = 5000))
     @Transactional(isolation = Isolation.READ_COMMITTED)
-    public FailedAnswerDto removeFailedAnswer(int failedAnswerId) {
-        return null;
-    }
+    public List<FailedAnswerDto> getFilteredFailedAnswers(int courseExecutionId, int dashboardId, int userId, String startDate, String endDate) {
+        //TODO
+        Student student = studentRepository.findById(userId).orElseThrow(() -> new TutorException(USER_NOT_FOUND, userId));
 
-    @Retryable(
-            value = {SQLException.class},
-            backoff = @Backoff(delay = 5000))
-    @Transactional(isolation = Isolation.READ_COMMITTED)
-    public List<FailedAnswerDto> reAddFailedAnswers(int courseExecutionId, int userId, String startDate, String endDate) {
-        return null;
-    }
+        Dashboard dashboard = dashboardRepository.findById(dashboardId).orElseThrow(() -> new TutorException(DASHBOARD_NOT_FOUND, dashboardId));
 
-    @Retryable(
-            value = {SQLException.class},
-            backoff = @Backoff(delay = 5000))
-    @Transactional(isolation = Isolation.READ_COMMITTED)
-    public List<FailedAnswerDto> getFilteredFailedAnswers(int courseExecutionId, int userId, String startDate, String endDate) {
+
         return null;
     }
 }
---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/repository/FailedAnswerRepository.java
@@ -0,0 +1,22 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.domain.FailedAnswer;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Repository
+@Transactional
+public interface FailedAnswerRepository extends JpaRepository<FailedAnswer, Integer> {
+
+    @Query(value = "SELECT fa.failed_answer_id FROM failed_answer JOIN dashboard da WHERE da.dashboard_id = :dashboardId AND fa.collected > :now", nativeQuery = true)
+    Set<Integer> findNewFailedAnswer(int dashboardId, LocalDateTime last);
+
+    @Query(value = "SELECT fa.failed_answer_id FROM failed_answer JOIN dashboard da WHERE da.dashboard_id = :dashboardId AND fa.collected BETWEEN :from AND :until", nativeQuery = true)
+    Set<Integer> findFailedAnswerFromDate(int dashboardId, LocalDateTime from, LocalDateTime until);
+}
+
