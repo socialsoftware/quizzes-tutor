39637eaae0b91b0cddbd5098fc878082f7f14915
JoaoGuerreiro <joao.guerreiro@eviso.it>
2022-02-27 20:04:27 +0000

    ERE 1.2: Update Current Weekly Score - Define Test Cases

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/service/UpdateWeeklyScoreTest.groovy
@@ -0,0 +1,131 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.service
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.MultipleChoiceAnswer
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.QuestionAnswer
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.QuizAnswer
+import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.domain.Dashboard
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.domain.WeeklyScore
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.MultipleChoiceQuestion
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Option
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.domain.Quiz
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.domain.QuizQuestion
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.dto.QuizDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Student
+import pt.ulisboa.tecnico.socialsoftware.tutor.utils.DateHandler
+import spock.lang.Unroll
+
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.temporal.TemporalAdjuster
+import java.time.temporal.TemporalAdjusters
+
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.DASHBOARD_NOT_FOUND
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.WEEKLY_SCORE_NOT_FOUND
+
+@DataJpaTest
+class UpdateWeeklyScoreTest extends SpockTest {
+
+    def student
+    def dashboard
+    def weeklyScore
+    def quiz
+    def quizQuestion
+    def optionOK
+    def OptionKO
+
+    def setup() {
+        createExternalCourseAndExecution()
+
+        student = new Student(USER_1_NAME, USER_1_USERNAME, USER_1_EMAIL, false, AuthUser.Type.EXTERNAL)
+        student.addCourse(externalCourseExecution)
+        userRepository.save(student)
+
+        dashboard = new Dashboard(externalCourseExecution, student)
+        dashboardRepository.save(dashboard)
+
+        weeklyScore = new WeeklyScore(dashboard)
+        TemporalAdjuster weekSunday = TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY);
+        LocalDate week = DateHandler.now().with(weekSunday).toLocalDate();
+        weeklyScore.setWeek(week)
+        weeklyScoreRepository.save(weeklyScore)
+
+        Question question = new Question()
+        question.setKey(1)
+        question.setTitle("Question Title")
+        question.setContent(QUESTION_1_CONTENT)
+        question.setStatus(Question.Status.AVAILABLE)
+        question.setCourse(externalCourse)
+        def questionDetails = new MultipleChoiceQuestion()
+        question.setQuestionDetails(questionDetails)
+        questionDetailsRepository.save(questionDetails)
+        questionRepository.save(question)
+
+        optionOK = new Option()
+        optionOK.setContent(OPTION_1_CONTENT)
+        optionOK.setCorrect(true)
+        optionOK.setSequence(0)
+        optionOK.setQuestionDetails(questionDetails)
+        optionRepository.save(optionOK)
+
+        optionKO = new Option()
+        optionKO.setContent(OPTION_1_CONTENT)
+        optionKO.setCorrect(false)
+        optionKO.setSequence(0)
+        optionKO.setQuestionDetails(questionDetails)
+        optionRepository.save(optionKO)
+
+        quiz = new Quiz()
+        quiz.setType(Quiz.QuizType.PROPOSED.toString())
+        quiz.setKey(1)
+        quiz.setCourseExecution(externalCourseExecution)
+        quizRepository.save(quiz)
+
+        quizQuestion= new QuizQuestion()
+        quizQuestion.setQuestion(question)
+        quizQuestion.setQuiz(quiz)
+        quizQuestionRepository.save(quizQuestion)
+    }
+
+    def "update weekly score"() {
+        given: "a correct question answer"
+
+        expect: true
+    }
+
+    def "update weekly score without answers in the current week"() {
+        when: "no answer is submitted"
+
+        expect: true
+    }
+
+    def "update weekly score with wrong answers in the current week"() {
+        given: "a wrong question answer"
+
+        expect: true
+    }
+
+    def "cannot update old WeeklyScore"() {
+        given: "old a weekly score"
+
+        expect: true
+    }
+
+    @Unroll
+    def "#test - cannot update WeeklyScore with weeklyScore=#weeklyScoreId"() {
+        when: "a weekly score is updated"
+
+        expect: true
+    }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
+}
