0d077270831fff8579a42bf7173dcd6d21b158a3
Antonio Rito Silva <rito.silva@tecnico.ulisboa.pt>
2022-06-02 16:07:08 +0100

    upgrade cypress 10.*

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'cypress';
+
+const { Client } = require('pg');
+
+async function queryDB(query: string, credentials: string) {
+  const client = new Client(credentials);
+  await client.connect();
+  const result = await client.query(query);
+  await client.end();
+
+  return result.rows;
+}
+
+export default defineConfig({
+  fixturesFolder: 'tests/e2e/fixtures',
+  projectId: '6y833w',
+  videoCompression: false,
+  screenshotsFolder: 'tests/e2e/screenshots',
+  videosFolder: 'tests/e2e/videos',
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('task', {
+        queryDatabase({ query, credentials }) {
+          return queryDB(query, credentials);
+        },
+      });
+    },
+    baseUrl: 'http://localhost:8081',
+    specPattern: 'tests/e2e/specs/**/*.{js,jsx,ts,tsx}',
+    supportFile: 'tests/e2e/support/index.js',
+  },
+});
