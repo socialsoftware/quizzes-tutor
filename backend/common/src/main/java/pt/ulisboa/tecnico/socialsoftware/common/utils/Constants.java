22fbe1fbee625551697ce3968a7f793ece513fd5
Pierre Véron <pierre.veron@outlook.fr>
2022-06-25 12:14:29 +0200

    Add constants

---
+++ backend/common/src/main/java/pt/ulisboa/tecnico/socialsoftware/common/utils/Constants.java
@@ -0,0 +1,8 @@
+package pt.ulisboa.tecnico.socialsoftware.common.utils;
+
+public class Constants {
+    public static final String DOMAIN = "test-domain";
+    public static final String TUTOR_TASK_LIST = "TutorTaskList";
+    public static final String AUTH_TASK_LIST = "AuthTaskList";
+    public static final String TOURNAMENT_TASK_LIST = "TournamentTaskList";
+}
