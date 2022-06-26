8f37dd8204a8d668768f7eef2696aaf595fa520f
Pierre Véron <pierre.veron@outlook.fr>
2022-06-26 12:23:22 +0200

    Implement UpdateTournamentActivities

--- backend/tournament/src/main/java/pt/ulisboa/tecnico/socialsoftware/tournament/activity/updateTournamentActivities/UpdateTournamentActivities.java
+++ backend/tournament/src/main/java/pt/ulisboa/tecnico/socialsoftware/tournament/activity/updateTournamentActivities/UpdateTournamentActivities.java
@@ -1,5 +1,20 @@
 package pt.ulisboa.tecnico.socialsoftware.tournament.activity.updateTournamentActivities;
 
+import java.util.Set;
+
+import pt.ulisboa.tecnico.socialsoftware.common.dtos.tournament.TournamentDto;
+import pt.ulisboa.tecnico.socialsoftware.tournament.domain.TournamentTopic;
+
 public interface UpdateTournamentActivities {
+
+    void beginUpdate(Integer tournamentId);
+
+    void undoUpdate(Integer tournamentId);
+
+    void updateTopics(Integer tournamentId, Set<TournamentTopic> topics);
+
+    void undoUpdateTopics(Integer tournamentId, Set<TournamentTopic> topics);
+
+    void confirmUpdate(Integer tournamentId, TournamentDto tournamentDto);
 
 }
--- backend/tournament/src/main/java/pt/ulisboa/tecnico/socialsoftware/tournament/activity/updateTournamentActivities/UpdateTournamentActivitiesImpl.java
+++ backend/tournament/src/main/java/pt/ulisboa/tecnico/socialsoftware/tournament/activity/updateTournamentActivities/UpdateTournamentActivitiesImpl.java
@@ -1,13 +1,43 @@
 package pt.ulisboa.tecnico.socialsoftware.tournament.activity.updateTournamentActivities;
 
+import java.util.Set;
+
+import pt.ulisboa.tecnico.socialsoftware.common.dtos.tournament.TournamentDto;
+import pt.ulisboa.tecnico.socialsoftware.tournament.domain.TournamentTopic;
 import pt.ulisboa.tecnico.socialsoftware.tournament.services.local.TournamentProvidedService;
 
-public class UpdateTournamentActivitiesImpl {
+public class UpdateTournamentActivitiesImpl implements UpdateTournamentActivities {
 
     private final TournamentProvidedService tournamentService;
 
     public UpdateTournamentActivitiesImpl(TournamentProvidedService tournamentService) {
         this.tournamentService = tournamentService;
+    }
+
+    @Override
+    public void beginUpdate(Integer tournamentId) {
+        tournamentService.beginUpdateTournament(tournamentId);
+    }
+
+    @Override
+    public void undoUpdate(Integer tournamentId) {
+        tournamentService.undoUpdate(tournamentId);
+    }
+
+    @Override
+    public void updateTopics(Integer tournamentId, Set<TournamentTopic> topics) {
+        tournamentService.updateTopics(tournamentId, topics);
+
+    }
+
+    @Override
+    public void undoUpdateTopics(Integer tournamentId, Set<TournamentTopic> topics) {
+        tournamentService.undoUpdateTopics(tournamentId, topics);
+    }
+
+    @Override
+    public void confirmUpdate(Integer tournamentId, TournamentDto tournamentDto) {
+        tournamentService.confirmUpdate(tournamentId, tournamentDto);
     }
 
 }
