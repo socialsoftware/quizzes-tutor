8a7bcbf2a83dc12f79975c68819d538b6e6591e1
Pierre Véron <pierre.veron@outlook.fr>
2022-06-28 21:14:20 +0100

    Add settings to tournament activities

--- backend/tournament/src/main/java/pt/ulisboa/tecnico/socialsoftware/tournament/activity/createTournamentActivities/CreateTournamentActivities.java
+++ backend/tournament/src/main/java/pt/ulisboa/tecnico/socialsoftware/tournament/activity/createTournamentActivities/CreateTournamentActivities.java
@@ -2,17 +2,24 @@ package pt.ulisboa.tecnico.socialsoftware.tournament.activity.createTournamentAc
 
 import java.util.Set;
 
+import com.uber.cadence.activity.ActivityMethod;
+
+import pt.ulisboa.tecnico.socialsoftware.common.utils.Constants;
 import pt.ulisboa.tecnico.socialsoftware.tournament.domain.TournamentCourseExecution;
 import pt.ulisboa.tecnico.socialsoftware.tournament.domain.TournamentTopic;
 
 public interface CreateTournamentActivities {
 
+    @ActivityMethod(scheduleToCloseTimeoutSeconds = 60, taskList = Constants.TOURNAMENT_TASK_LIST)
     void undoCreate(Integer tournamentId);
 
+    @ActivityMethod(scheduleToCloseTimeoutSeconds = 60, taskList = Constants.TOURNAMENT_TASK_LIST)
     void confirmCreate(Integer tournamentId, Integer quizId);
 
+    @ActivityMethod(scheduleToCloseTimeoutSeconds = 60, taskList = Constants.TOURNAMENT_TASK_LIST)
     void storeCourseExecution(Integer tournamentId, TournamentCourseExecution tournamentCourseExecution);
 
+    @ActivityMethod(scheduleToCloseTimeoutSeconds = 60, taskList = Constants.TOURNAMENT_TASK_LIST)
     void storeTopics(Integer tournamentId, Set<TournamentTopic> tournamentTopics);
 
 }
--- backend/tournament/src/main/java/pt/ulisboa/tecnico/socialsoftware/tournament/activity/removeTournamentActivities/RemoveTournamentActivities.java
+++ backend/tournament/src/main/java/pt/ulisboa/tecnico/socialsoftware/tournament/activity/removeTournamentActivities/RemoveTournamentActivities.java
@@ -1,11 +1,18 @@
 package pt.ulisboa.tecnico.socialsoftware.tournament.activity.removeTournamentActivities;
 
+import com.uber.cadence.activity.ActivityMethod;
+
+import pt.ulisboa.tecnico.socialsoftware.common.utils.Constants;
+
 public interface RemoveTournamentActivities {
 
+    @ActivityMethod(scheduleToCloseTimeoutSeconds = 60, taskList = Constants.TOURNAMENT_TASK_LIST)
     void beginRemove(Integer tournamentId);
 
+    @ActivityMethod(scheduleToCloseTimeoutSeconds = 60, taskList = Constants.TOURNAMENT_TASK_LIST)
     void undoRemove(Integer tournamentId);
 
+    @ActivityMethod(scheduleToCloseTimeoutSeconds = 60, taskList = Constants.TOURNAMENT_TASK_LIST)
     void confirmRemove(Integer tournamentId);
 
 }
