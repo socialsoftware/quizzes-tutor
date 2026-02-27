2c4ce9e910e2f9067f261f8a7b09be7986323b4a
Antonio Rito Silva <rito.silva@tecnico.ulisboa.pt>
2026-02-27 15:50:01 +0000

    fix(frontend): top bar

--- frontend/src/vuetify.ts
+++ frontend/src/vuetify.ts
@@ -32,8 +32,6 @@ export default createVuetify({
   defaults: {
     VBtn: {
       style: 'text-transform: uppercase; letter-spacing: 0.0892857143em;',
-      size: 'small',
-      height: 36,
     },
     VTextField: {
       variant: 'underlined',
